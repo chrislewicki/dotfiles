a7731f8b1c8db222894a05a66136413f0613e1fc
Chris Lewicki <chris@lewicki.dev>
2020-04-11 15:43:50 -0400

    slimmer scripts for more disposable linux environments

---
+++ litedeploy.sh
@@ -0,0 +1,1 @@
+sudo apt-get -y install neovim zsh htop tmux curl wget python3-dev python3-pip && ln -s /home/$USER/dotfiles/.zshrc /home/$USER/.zshrc && ln -s /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf
