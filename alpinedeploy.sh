8d3197958f129ee35708d76109f0043c5f14c657
Chris Lewicki <chris@lewicki.dev>
2021-09-01 10:03:57 -0400

    add deployment script for alpine linuxes

---
+++ alpinedeploy.sh
@@ -0,0 +1,1 @@
+sudo apk add vim zsh htop tmux curl wget && ln -s /home/$USER/dotfiles/.zshrc /home/$USER/.zshrc && ln -s /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf && ln -s /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
